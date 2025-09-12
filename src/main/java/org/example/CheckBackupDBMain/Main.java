17dc72531c0b7fdcdb0ae93732a525b17513854e
Leonid Pintea <leonid.pintea@ness.com>
2025-09-12 16:02:12 +0300

    creare log scris

--- src/main/java/org/example/CheckBackupDBMain/Main.java
+++ src/main/java/org/example/CheckBackupDBMain/Main.java
@@ -1,15 +1,26 @@
 package org.example.CheckBackupDBMain;
 
 import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.logging.FileHandler;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Main {
     private static int countNumberOfFiles = 0;
     private static Logger logger = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
+        try{
+            FileHandler fileHandler = new FileHandler("backupCleaner_job.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+        }catch(IOException ex){
+            System.err.println("Error in log_job_backupCleaner.log " + ex.getMessage());
+            ex.printStackTrace();
+        }
         String backupDir = "C:\\Users\\Leonid Pintea\\Documents\\CheckBackupDB_TEST";
         File[] folderContent = new File(backupDir).listFiles();//stocam fisierele din folder
         for(File f : folderContent) {
@@ -33,24 +44,17 @@ public class Main {
     public static long findDiffDaysFromToday(File currentFile){//Aceasta functie primeste un fisier si ne spune cat de vechi este (in zile) fata de ziua curenta
         //seteaza formatul de data yyyyMMdd
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-        //taie numele de la caracterul _ pana la . si stocheaza doar "20250801"
-        //String datePart = currentFile.getName().substring(currentFile.getName().indexOf("_") + 1, currentFile.getName().indexOf("."));
-        //LocalDate fileDate = LocalDate.parse(datePart, formatter); // fileDate = data fisierului in tip obiect LocalDate
         LocalDate todayDate = LocalDate.now(); // todayDate = data curenta in tip obiect LocalDate. Aceste doua variabile sunt create pentru a fi folosite in ChronoUnit
+        String[] datePart = currentFile.getName().split("\\."); //luam al 3-lea string din array care va fi data
+        LocalDate fileDate = LocalDate.parse(datePart[2], formatter);
 
-
-        String[] datePartFinal = currentFile.getName().split("\\.");
-        LocalDate fileDateFinal = LocalDate.parse(datePartFinal[2], formatter);
-
-        //return ChronoUnit.DAYS.between(fileDate, todayDate);
-        return ChronoUnit.DAYS.between(fileDateFinal, todayDate);
+        return ChronoUnit.DAYS.between(fileDate, todayDate);
     }
 
 
     public static boolean deleteFile(File file){
         //Stergem fisierul daca exista
         if (file.exists()) {
-            //System.out.println("Stergem fisierul " + file.getName());
             logger.info("Stergem fisierul " + file.getName());
             return file.delete();
         }
