4be8e7a8105e9688e86a802792db60b237f37196
Leonid Pintea <leonid.pintea@ness.com>
2025-09-12 14:19:38 +0300

    s-a adaugat maven pentru creare .jar; +log

--- src/main/java/org/example/CheckBackupDBMain/Main.java
+++ src/main/java/org/example/CheckBackupDBMain/Main.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.logging.Logger;
 
 public class Main {
+    private static int countNumberOfFiles = 0;
+    private static Logger logger = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
         String backupDir = "C:\\Users\\Leonid Pintea\\Documents\\CheckBackupDB_TEST";
         File[] folderContent = new File(backupDir).listFiles();//stocam fisierele din folder
@@ -13,14 +16,21 @@ public class Main {
             if(f.isFile()){
                 if(findDiffDaysFromToday(f) > 14){
                     System.out.println(f.getName());
+                    countNumberOfFiles++;
                     //deleteFile(f);
+                    logger.info("S-a sters fisierul " + f.getName());
                 }
             }
         }
+        if (countNumberOfFiles != 9) {
+            logger.severe("Job-ul a esuat!");
+        }else {
+            logger.info("Succes!");
+        }
+        countNumberOfFiles = 0;
     }
 
     public static long findDiffDaysFromToday(File currentFile){//Aceasta functie primeste un fisier si ne spune cat de vechi este (in zile) fata de ziua curenta
-        //ToDo de verificat daca fisierul respecta sau are formatul de data in el, altfel arunca exceptie
         //seteaza formatul de data yyyyMMdd
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
         //taie numele de la caracterul _ pana la . si stocheaza doar "20250801"
@@ -40,14 +50,10 @@ public class Main {
     public static boolean deleteFile(File file){
         //Stergem fisierul daca exista
         if (file.exists()) {
-            System.out.println("Stergem fisierul " + file.getName());
+            //System.out.println("Stergem fisierul " + file.getName());
+            logger.info("Stergem fisierul " + file.getName());
             return file.delete();
         }
         return false;
-    }
-
-    public static boolean checkFileName(File file){
-        //ToDo verificam daca fisierul are formatul prestabilit. Pentur fiecare client probabil vom avea fisiere cu formate diferite
-        return true;
     }
 }
